2bbef0faa50deb5bcd86c2cba7ed0b2479fba5f0
birkin <birkin.diana@gmail.com>
2019-11-11 09:40:10 -0500

    initial commit.

---
+++ config/urls.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import RedirectView
+from mp_vl_app import views
+
+
+admin.autodiscover()
+
+
+urlpatterns = [
+
+    ## primary app urls...
+    url( r'^admin/', admin.site.urls ),  # eg host/project_x/admin/
+    # url( r'^admin/login/', RedirectView.as_view(pattern_name='login_url') ),
+    # url( r'^login/$', views.login, name='login_url' ),
+
+    ## support urls...
+    url( r'^version/$', views.version, name='version_url' ),
+    url( r'^error_check/$', views.error_check, name='error_check_url' ),
+
+    url( r'^$', RedirectView.as_view(pattern_name='version_url') ),
+
+    ]
---
+++ mp_vl_app/views.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import datetime, json, logging, os, pprint
+from . import settings_app
+from mp_vl_app.lib import view_info_helper
+# from mp_vl_app.lib.shib_auth import shib_login  # decorator
+from django.conf import settings as project_settings
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+
+
+log = logging.getLogger(__name__)
+
+
+# def dummy_url_handler( request ):
+#     """ Docstring. """
+#     pass
+
+
+# ===========================
+# for development convenience
+# ===========================
+
+
+def version( request ):
+    """ Returns basic data including branch & commit. """
+    # log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
+    rq_now = datetime.datetime.now()
+    commit = view_info_helper.get_commit()
+    branch = view_info_helper.get_branch()
+    info_txt = commit.replace( 'commit', branch )
+    resp_now = datetime.datetime.now()
+    taken = resp_now - rq_now
+    context_dct = view_info_helper.make_context( request, rq_now, info_txt, taken )
+    output = json.dumps( context_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
+
+
+def error_check( request ):
+    """ For an easy way to check that admins receive error-emails (in development).
+        To view error-emails in runserver-development:
+        - run, in another terminal window: `python -m smtpd -n -c DebuggingServer localhost:1026`,
+        - (or substitue your own settings for localhost:1026)
+    """
+    if project_settings.DEBUG == True:
+        1/0
+    else:
+        return HttpResponseNotFound( '<div>404 / Not Found</div>' )
+
+
+# @shib_login
+# def login( request ):
+#     """ Handles authNZ, & redirects to admin.
+#         Called by click on login or admin link. """
+#     next_url = request.GET.get( 'next', None )
+#     if not next_url:
+#         redirect_url = reverse( settings_app.POST_LOGIN_ADMIN_REVERSE_URL )
+#     else:
+#         redirect_url = request.GET['next']  # will often be same page
+#     log.debug( 'redirect_url, ```%s```' % redirect_url )
+#     return HttpResponseRedirect( redirect_url )
