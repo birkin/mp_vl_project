dc709ea04c005efee0d6ff167da9edb55f3f5b4e
birkin <birkin.diana@gmail.com>
2019-11-27 09:38:35 -0500

    adds working dev-server demo-code.

--- mp_vl_app/lib/mongo_access.py
+++ mp_vl_app/lib/mongo_access.py
@@ -19,10 +19,65 @@ password = urllib.parse.quote_plus( PASS )
 connect_str = f'mongodb://{username}:{password}@{HOST}:{PORT}/'
 log.debug( f'connect_str, ```{connect_str}```' )
 
-connect_str_2 = f'{connect_str}?authSource={DB_NAME}&authMechanism=SCRAM-SHA-256'
-log.debug( f'connect_str, ```{connect_str}```' )
+# connect_str_2 = f'{connect_str}?authSource={DB_NAME}&authMechanism=SCRAM-SHA-256'
+# log.debug( f'connect_str, ```{connect_str}```' )
 
 client = MongoClient( connect_str )
 print( f'client, ```{client}```' )
 
 print( f'client, ```{pprint.pformat(client.__dict__)}```' )
+
+
+# ----------------------------------
+# working dev-server demo-code below
+# ----------------------------------
+
+# import os, urllib.parse
+# import pymongo
+
+# M_HOST = os.environ['MV_DJ__MONGO_HOST']
+# M_PORT = os.environ['MV_DJ__MONGO_PORT']
+# M_DB = os.environ['MV_DJ__MONGO_DATABASE_NAME']
+# M_COLL = os.environ['MV_DJ__MONGO_COLLECTION_NAME']
+# M_USER =  os.environ['MV_DJ__MONGO_USERNAME']
+# M_PASS =  os.environ['MV_DJ__MONGO_PASSWORD']
+
+# username = urllib.parse.quote_plus( M_USER )
+# password = urllib.parse.quote_plus( M_PASS )
+
+# connect_str_1 = f'mongodb://{username}:{password}@{M_HOST}:{M_PORT}/'
+# print( f'connect_str_1, ```{connect_str_1}```' )
+
+# connect_str_2 = f'{connect_str_1}?authSource={M_DB}'
+# print( f'connect_str_2, ```{connect_str_2}```' )
+
+# m_client = pymongo.MongoClient( connect_str_2 )
+# m_db = m_client[M_DB]
+# m_collection = m_db[M_COLL]
+
+# x = m_collection.find_one()
+
+# print( x )
+
+
+# --------------------------------
+# working localdev demo-code below
+# --------------------------------
+
+# import os
+# import pymongo
+
+# M_HOST = os.environ['MV_DJ__MONGO_HOST']
+# M_PORT = os.environ['MV_DJ__MONGO_PORT']
+# M_DB = os.environ['MV_DJ__MONGO_DATABASE_NAME']
+# M_COLL = os.environ['MV_DJ__MONGO_COLLECTION_NAME']
+
+# connect_str = f'mongodb://{M_HOST}:{M_PORT}/'
+
+# m_client = pymongo.MongoClient( connect_str )
+# m_db = m_client[M_DB]
+# m_collection = m_db[M_COLL]
+
+# x = m_collection.find_one()
+
+# print( x )
--- mp_vl_app/settings_app.py
+++ mp_vl_app/settings_app.py
@@ -11,3 +11,11 @@ STAFF_USERS = json.loads( os.environ['MV_DJ__STAFF_USERS_JSON'] )  # users permi
 STAFF_GROUP = os.environ['MV_DJ__STAFF_GROUP']  # not grouper-group; rather, name of django-admin group for permissions
 TEST_META_DCT = json.loads( os.environ['MV_DJ__TEST_META_DCT_JSON'] )
 # POST_LOGIN_ADMIN_REVERSE_URL = os.environ['MV_DJ__POST_LOGIN_ADMIN_REVERSE_URL']  # tricky; for a direct-view of a model, the string would be in the form of: `admin:APP-NAME_MODEL-NAME_changelist`
+
+## mongo
+DB_HOST = os.environ['MV_DJ__MONGO_HOST']
+DB_PORT =  os.environ['MV_DJ__MONGO_PORT']
+DB_NAME = os.environ['MV_DJ__MONGO_DATABASE_NAME']
+DB_ENTRIES = os.environ['MV_DJ__MONGO_ENTRIES_COLLECTION_NAME']
+DB_USER =  os.environ['MV_DJ__MONGO_USERNAME']
+DB_PASS =  os.environ['MV_DJ__MONGO_PASSWORD']
