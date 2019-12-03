e6eda0605b3bc28669a0e9eed336230e837a8255
birkin <birkin.diana@gmail.com>
2019-12-03 16:09:15 -0500

    refactors date-display prep.

--- mp_vl_app/lib/views_api_entries_helper.py
+++ mp_vl_app/lib/views_api_entries_helper.py
@@ -23,15 +23,9 @@ def massage_doc_data( doc_dct ):
 def stringify_date( date_dct ):
     """ Converts given date-fields to str.
         Called by massage_doc_data() """
-    date_str = ''
-    if not date_dct:
-        return date_str
-    year, month, day, modifier = date_dct.get('year', None), date_dct.get('month', None), date_dct.get('day', None), date_dct.get('modifier', None)
-    month = intify_month( date_dct['month'])
-    year_as_bool = True if year else False
-    month_as_bool = True if month else False
-    day_as_bool = True if day else False
-    modifier_as_bool = True if modifier else False
+    if not date_dct: return ''
+    date_str = 'INVALID DATE'
+    ( year, month, day, modifier, year_as_bool, month_as_bool, day_as_bool, modifier_as_bool ) = initialize_date_data( date_dct )
     if ( year_as_bool and month and modifier_as_bool and day ):
         date_str = f'{year} {month} {day} ({modifier})'
     elif ( year_as_bool and month_as_bool and day_as_bool and not modifier_as_bool ):
@@ -44,20 +38,38 @@ def stringify_date( date_dct ):
         date_str = f'{modifier} {year}'
     elif ( year_as_bool and not modifier_as_bool and not month_as_bool and not day_as_bool ):
         date_str = f'{year}'
-    else:
-        date_str = 'INVALID DATE'
     return date_str
+
+
+def initialize_date_data( date_dct ):
+    """ Initializes vars.
+        Called by stringify_date() """
+    log.debug( f'date_dct, ```{date_dct}```' )
+    year, month, day, modifier = (
+        date_dct.get('year', None), date_dct.get('month', None), date_dct.get('day', None), date_dct.get('modifier', None) )
+    month = intify_month( date_dct['month'] )
+    year_as_bool = True if year else False
+    month_as_bool = True if month else False
+    day_as_bool = True if day else False
+    modifier_as_bool = True if modifier else False
+    log.debug( f'year, `{year}`; month, `{month}`; day, `{day}`; modifier, `{modifier}`; year_as_bool, `{year_as_bool}`; month_as_bool, `{month_as_bool}`; day_as_bool, `{day_as_bool}`; modifier_as_bool, `{modifier_as_bool}`' )
+    return ( year, month, day, modifier, year_as_bool, month_as_bool, day_as_bool, modifier_as_bool )
 
 
 def intify_month( num ):
     """ Converts given month number to appropriate string.
-        Called by stringify_date() """
+        Called by initialize_date_data() """
+    log.debug( f'num, `{num}`; type(num), `{type(num)}`' )
     month_names = [
         'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
     month_int = month_names[ int(num) - 1 ]
     log.debug( f'month_int, `{month_int}`' )
     return month_int
 
+
+# -----------------------------------
+# C.'s js code to create display-date
+# -----------------------------------
 
 # function monthAsNumberToString(num) {
 #     const monthNames = [
@@ -84,7 +96,7 @@ def intify_month( num ):
 #         string = `${year} ${month} ${modifier} ${day}`;
 #     } else if (yearAsBool && monthAsBool && dayAsBool && !modifierAsBool) {
 #         string = `${year} ${month} ${day}`;
-#     } else if (yearAsBool & modifierAsBool && monthAsBool && !dayAsBool) {
+#     } else if (yearAsBool & modifierAsBool && monthAsBool && !dayAsBool) {  // BJD note: I think the `&` should've been a `&&`
 #         string = `${year} ${modifier} ${month}`;
 #     } else if (yearAsBool && !modifierAsBool && monthAsBool && !dayAsBool) {
 #         string = `${year} ${month}`;
