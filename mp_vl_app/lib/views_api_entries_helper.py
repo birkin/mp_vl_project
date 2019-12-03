b6dbc9622ef7b62a3209b6ae8eb959a1864d2bac
birkin <birkin.diana@gmail.com>
2019-12-03 15:51:19 -0500

    calculates date-display.

--- mp_vl_app/lib/views_api_entries_helper.py
+++ mp_vl_app/lib/views_api_entries_helper.py
@@ -7,8 +7,9 @@ log = logging.getLogger(__name__)
 def massage_doc_data( doc_dct ):
     """ Updates doc data so it can be jsonized.
         Called by views.api_entries() """
+    log.debug( f'initial doc_dct, ```{pprint.pformat(doc_dct)}```' )
     doc_dct['_id'] = str( doc_dct['_id'] )
-    doc_dct['date'] = 'foo'
+    doc_dct['date_display'] = stringify_date( doc_dct['date'] )
     if 'metadata' in doc_dct.keys():
         if 'lastEditedAt' in doc_dct['metadata'].keys():
             doc_dct['metadata']['lastEditedAt'] = str( doc_dct['metadata']['lastEditedAt'] )
@@ -21,8 +22,41 @@ def massage_doc_data( doc_dct ):
 
 def stringify_date( date_dct ):
     """ Converts given date-fields to str.
-        Called by views.api_entries() """
-    return 'foo'
+        Called by massage_doc_data() """
+    date_str = ''
+    if not date_dct:
+        return date_str
+    year, month, day, modifier = date_dct.get('year', None), date_dct.get('month', None), date_dct.get('day', None), date_dct.get('modifier', None)
+    month = intify_month( date_dct['month'])
+    year_as_bool = True if year else False
+    month_as_bool = True if month else False
+    day_as_bool = True if day else False
+    modifier_as_bool = True if modifier else False
+    if ( year_as_bool and month and modifier_as_bool and day ):
+        date_str = f'{year} {month} {day} ({modifier})'
+    elif ( year_as_bool and month_as_bool and day_as_bool and not modifier_as_bool ):
+        date_str = f'{year} {month} {day}'
+    elif ( year_as_bool and modifier_as_bool and month_as_bool and not day_as_bool ):
+        date_str = f'{year} {month} ({modifier})'
+    elif ( year_as_bool and not modifier_as_bool and month_as_bool and not day_as_bool ):
+        date_str = f'{year} {month}'
+    elif ( year_as_bool and modifier_as_bool and not month_as_bool and not day_as_bool ):
+        date_str = f'{modifier} {year}'
+    elif ( year_as_bool and not modifier_as_bool and not month_as_bool and not day_as_bool ):
+        date_str = f'{year}'
+    else:
+        date_str = 'INVALID DATE'
+    return date_str
+
+
+def intify_month( num ):
+    """ Converts given month number to appropriate string.
+        Called by stringify_date() """
+    month_names = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+    month_int = month_names[ int(num) - 1 ]
+    log.debug( f'month_int, `{month_int}`' )
+    return month_int
 
 
 # function monthAsNumberToString(num) {
--- mp_vl_app/lib/views_dblist_helper.py
+++ mp_vl_app/lib/views_dblist_helper.py
@@ -22,24 +22,7 @@ def build_data( scheme, host, user ):
     log.debug( f'api_url, ```{api_url}```' )
     r = requests.get( api_url )
     data = r.json()
-    updated_data = data.copy()
-    for record in updated_data:
-        log.debug( f'record, ```{pprint.pformat(record)}```' )
-        dt_obj, dt_display = None, None
-        try:
-            date_dct = record['date']
-            dt_obj = datetime.date( date_dct['year'], date_dct['month'], date_dct['day'] )
-        except:
-            pass
-        if dt_obj:
-            dt_display = dt_obj.strftime( '%Y %B %d' )
-            if 'modifier' in date_dct.keys():
-                mod_value = date_dct['modifier'].lower()
-                dt_display = f'{dt_display} ({mod_value})'
-        else:
-            dt_display = 'INVALID_DATE'
-        record['date_display'] = dt_display
-    context = { 'data': updated_data }
+    context = { 'data': data }
     username = None
     if user.is_authenticated:
         username = user.first_name
@@ -49,67 +32,3 @@ def build_data( scheme, host, user ):
     context['username'] = username
     log.debug( f'context.keys(), ```{pprint.pformat(context.keys())}```' )
     return context
-
-
-# def make_context( request, rq_now, info_txt, taken ):
-#     """ Builds and returns context.
-#         Called by views.info() """
-#     cntxt = {
-#         'request': {
-#             'url': '%s://%s%s' % ( request.scheme,
-#                 request.META.get( 'HTTP_HOST', '127.0.0.1' ),  # HTTP_HOST doesn't exist for client-tests
-#                 request.META.get('REQUEST_URI', request.META['PATH_INFO'])
-#                 ),
-#             'timestamp': str( rq_now )
-#         },
-#         'response': {
-#             'documentation': settings_app.README_URL,
-#             'version': info_txt,
-#             'elapsed_time': str( taken )
-#         }
-#     }
-#     return cntxt
-
-
-# --------------------
-# from js code...
-# --------------------
-
-# function monthAsNumberToString(num) {
-#     const monthNames = [
-#         "January", "February", "March",
-#         "April", "May", "June", "July",
-#         "August", "September", "October",
-#         "November", "December"
-#     ];
-#     return monthNames[num-1];
-# }
-
-# export default (date) => {
-#     let string;
-#     if (!date) {
-#         return;
-#     }
-#     let { year, month, day, modifier } = date;
-#     month = monthAsNumberToString(month);
-#     const yearAsBool = !!year;
-#     const monthAsBool = !!month;
-#     const modifierAsBool = !!modifier;
-#     const dayAsBool = !!day;
-#     if (yearAsBool && month && modifierAsBool && day) {
-#         string = `${year} ${month} ${modifier} ${day}`;
-#     } else if (yearAsBool && monthAsBool && dayAsBool && !modifierAsBool) {
-#         string = `${year} ${month} ${day}`;
-#     } else if (yearAsBool & modifierAsBool && monthAsBool && !dayAsBool) {
-#         string = `${year} ${modifier} ${month}`;
-#     } else if (yearAsBool && !modifierAsBool && monthAsBool && !dayAsBool) {
-#         string = `${year} ${month}`;
-#     } else if (yearAsBool && modifierAsBool && !monthAsBool && !dayAsBool) {
-#         string = `${modifier} ${year}`;
-#     } else if (yearAsBool && !modifierAsBool && !monthAsBool && !dayAsBool) {
-#         string = `${year}`;
-#     } else {
-#         string = "INVALID DATE";
-#     }
-#     return string;
-# }
