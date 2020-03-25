66d8af56323b85f0b1b397723a2d6e7395112181
birkin <birkin.diana@gmail.com>
2020-03-25 11:44:18 -0400

    adds tool foundation and derived files.

---
+++ mp_vl_app/static/experimentation_04/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require( 'path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    watch: true,
+    devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.js' ]
+    }
+
+}
