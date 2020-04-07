e4589c48218d6c8a7a24737463c8ba89422604df
birkin <birkin.diana@gmail.com>
2020-04-07 08:39:23 -0400

    gets the 05 static tree working.

--- mp_vl_app/static/experimentation_05/src/index.js
+++ mp_vl_app/static/experimentation_05/src/index.js
@@ -2,36 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked comment number ' + this.props.commentID;
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-// Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-    ReactDOM.render(
-      e(LikeButton, { commentID: commentID }),
-      domContainer
-    );
-  });
+ReactDOM.render(
+    <div>
+        <h1>This (05) is from JSX!!</h1>
+    </div>,
+    document.getElementById("root")
+)
