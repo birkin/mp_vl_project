ecbffd20a19cc4c048dc7a40ed2a15a519bb9f75
birkin <birkin.diana@gmail.com>
2020-05-08 09:57:38 -0400

    adds debug logging.

--- mp_vl_app/lib/views_dblist_helper.py
+++ mp_vl_app/lib/views_dblist_helper.py
@@ -18,22 +18,26 @@ def build_data( scheme: str, host: str, user: django.utils.functional.SimpleLazy
         Note: decision... I could make the api call return non-jsonized mongo data,
             and have this function process it, but I'm going to assume an api should directly return json,
             and so will handle the object-to-json conversion in views.api_entries() """
-    log.debug( f'host, `{host}`' )
-    api_url = f'{scheme}://{host}{reverse("api_entries_url")}'
-    log.debug( f'api_url, ```{api_url}```' )
-    r = requests.get( api_url, timeout=10 )
-    data: List(dict) = r.json()
-    context = { 'data': data }
-    username = None
-    if user.is_authenticated:  # `user` becomes `django.contrib.auth.models.User` or `...AnonymousUser`
-        username: str = user.first_name
-        context['logged_in'] = True
-        context['entry_url'] = settings_app.ENTRY_URL
-        context['entry_version_url'] = settings_app.ENTRY_VERSION_URL
-        context['new_entry_url'] = settings_app.NEW_ENTRY_URL
-    else:
-        context['logged_in'] = False
-    context['username'] = username
-    context['time_taken'] = str( datetime.datetime.now() - start_time )
-    log.debug( f'context.keys(), ```{pprint.pformat(context.keys())}```' )
-    return context
+    try:
+        log.debug( f'host, `{host}`' )
+        api_url = f'{scheme}://{host}{reverse("api_entries_url")}'
+        log.debug( f'api_url, ```{api_url}```' )
+        r = requests.get( api_url, timeout=10 )
+        data: List(dict) = r.json()
+        context = { 'data': data }
+        username = None
+        if user.is_authenticated:  # `user` becomes `django.contrib.auth.models.User` or `...AnonymousUser`
+            username: str = user.first_name
+            context['logged_in'] = True
+            context['entry_url'] = settings_app.ENTRY_URL
+            context['entry_version_url'] = settings_app.ENTRY_VERSION_URL
+            context['new_entry_url'] = settings_app.NEW_ENTRY_URL
+        else:
+            context['logged_in'] = False
+        context['username'] = username
+        context['time_taken'] = str( datetime.datetime.now() - start_time )
+        log.debug( f'context.keys(), ```{pprint.pformat(context.keys())}```' )
+        return context
+    except:
+        log.exception( 'problem building data; traceback follows' )
+        raise Exception( 'check logs' )
